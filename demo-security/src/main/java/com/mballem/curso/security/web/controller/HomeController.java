52f3b417acde5c737d95d17ba0eab98e9d24f559
MD <MD@DESKTOP-SP5THVE>
2023-03-16 15:20:55 -0300

    Sesão 2 - Iniciando com Spring Security (Concluida)

--- demo-security/src/main/java/com/mballem/curso/security/web/controller/HomeController.java
+++ demo-security/src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.mballem.curso.security.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	// abrir pagina home
-	@GetMapping("/")
+	@GetMapping({ "/", "/home" })
 	public String home() {
 		return "home";
-	}	
+	}
+
+	// Abrir página login
+	@GetMapping({ "/login" })
+	public String login() {
+		return "login";
+	}
+
+	// Login inválido
+	@GetMapping({ "/login-error" })
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais inválidas");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+		return "login";
+	}
 }
